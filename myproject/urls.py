bea2a7b3dedcd57dfc832669c1ec9196668fabe2
ydurra <yuridurra@gmail.com>
2025-04-25 10:37:43 -0300

    Ajustes

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,4 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
+    path('api/', include('minhaapp.api_urls')),  # URLs da API
+    path('api-auth/', include('rest_framework.urls')),  # Login para a API
+    
 ]
