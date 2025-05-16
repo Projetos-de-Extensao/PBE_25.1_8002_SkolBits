a80449aef0ce0458e67a777de842df90f32c3e62
fabriciobrito01 <fabriciodbe@gmail.com>
2025-05-16 08:05:45 -0300

    updates api

---
+++ src/Censo/myapp/api_urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from myapp.api import ProdutoViewSet, ProdutosBaratosAPIView
+
+router = DefaultRouter()
+router.register(r'produtos', ProdutoViewSet, basename='produto')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('produtos/baratos/', ProdutosBaratosAPIView.as_view(), name='produtos-baratos'),
+]
