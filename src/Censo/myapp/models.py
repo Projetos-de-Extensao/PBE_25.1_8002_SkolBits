35f278c4b48ddfb4b185e4560d3bfa8bb60dde81
fabriciobrito01 <fabriciodbe@gmail.com>
2025-05-22 23:14:52 -0300

    add especie domicilio

--- src/Censo/myapp/models.py
+++ src/Censo/myapp/models.py
@@ -12,5 +12,13 @@ class IdentificacaoDomicilio(models.Model):
     nr_quadra = models.CharField(max_length=3)
     nr_da_face = models.CharField(max_length=3)
 
+    ESPECIE_CHOICES = [
+        ('DPP', 'Domicílio particular permanente ocupado'),
+        ('DPI', 'Domicílio particular improvisado ocupado'),
+        ('DCM', 'Domicílio coletivo com morador'),
+    ]
+    tipo = models.CharField(max_length=3, choices=ESPECIE_CHOICES, default='DPP')
+
+
     def __str__(self):
-        return self.uf
+        return f"Domicílio em {self.municipio}, UF: {self.uf}"
