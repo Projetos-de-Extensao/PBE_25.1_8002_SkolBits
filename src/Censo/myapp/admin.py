d4a17ed4fe60aa8ccf315d7edeec6e4448eb27de
Lucas Kronemberger <dev.kronemberger@gmail.com>
2025-05-22 23:52:04 -0300

    add info admin py

--- src/Censo/myapp/admin.py
+++ src/Censo/myapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import IdentificacaoDomicilio
+from .models import IdentificacaoDomicilio, InformacoesMoradores
 
 admin.site.register(IdentificacaoDomicilio)
+admin.site.register(InformacoesMoradores)
