7da9d2e53ae2e66b0ee305604cf22b8146895274
Yago Duarte <yagoduarte1005@gmail.com>
2025-06-06 22:28:31 -0300

    Update info moradores

--- src/Censo/myapp/models.py
+++ src/Censo/myapp/models.py
@@ -52,139 +52,108 @@ class InformacoesMoradores (models.Model):
     data_nascimento = models.DateField(verbose_name="Data de nascimento")
 
     RELACAO_COM_RESPONSAVEL_CHOICES = [
-        ('001', 'Pessoa responsável pelo domicilio')
-        ('002', 'Cônjuge ou companheiro(a) de sexo diferente')
-        ('003', 'Cônjuge ou companheiro(a) do mesmo sexo')
-        ('004', 'Filho(a) do responsável e do cônjuge')
-        ('005', 'Filho(a) somente do responsável')
-        ('006', 'Pessoa responsável pelo domicílio')
-        ('007', 'Genro ou nora')
-        ('008', 'Pai, mãe, padrasto ou madrasta')
-        ('009', 'Sogro(a)')
-        ('010', 'Neto(a)')
-        ('011', 'Enteado(a)')
-        ('012', 'Irmão ou irmã')
-        ('013', 'Avô ou avó')
-        ('014', 'Outro parente')
-        ('015', 'Agregado(a)')
-        ('016', 'Bisneto(a)')
-        ('017', 'Pensionista')
-        ('018', 'Empregado(a) doméstico(a)')
-        ('019', 'Parente do(a) empregado(a) doméstico(a)')
-        ('020', 'Individual em domicílio coletivo')
+        ('pessoa_responsavel_pelo_domicilio', 'Pessoa responsável pelo domicilio')
+        ('conjuge_ou_companheiro(a)_de_sexo_diferente', 'Cônjuge ou companheiro(a) de sexo diferente')
+        ('conjuge_ou_companheiro(a)_do_mesmo_sexo', 'Cônjuge ou companheiro(a) do mesmo sexo')
+        ('filho(a)_do_responsavel_e_do_conjuge', 'Filho(a) do responsável e do cônjuge')
+        ('filho(a)_somente_do_responsavel', 'Filho(a) somente do responsável')
+        ('pessoa_responsavel_pelo_domicilio', 'Pessoa responsável pelo domicílio')
+        ('genro_ou_nora', 'Genro ou nora')
+        ('pai_mae_padrasto_ou_madrasta', 'Pai, mãe, padrasto ou madrasta')
+        ('sogro(a)', 'Sogro(a)')
+        ('neto(a)', 'Neto(a)')
+        ('enteado(a)', 'Enteado(a)')
+        ('irmão_ou_irma', 'Irmão ou irmã')
+        ('avô_ou_avo', 'Avô ou avó')
+        ('outroparente', 'Outro parente')
+        ('agregado(a)', 'Agregado(a)')
+        ('bisneto(a)', 'Bisneto(a)')
+        ('pensionista', 'Pensionista')
+        ('empregado(a)_domestico(a)', 'Empregado(a) doméstico(a)')
+        ('parente_do(a)_empregado(a)_domestico(a)', 'Parente do(a) empregado(a) doméstico(a)')
+        ('individual_em_domicilio_coletivo', 'Individual em domicílio coletivo')
     ]
-    relacao_com_responsavel = models.CharField(max_length=3, choices=RELACAO_COM_RESPONSAVEL_CHOICES, verbose_name="Relação com responsável")
+    relacao_com_responsavel = models.CharField(max_length=3, choices=RELACAO_COM_RESPONSAVEL_CHOICES, verbose_name="Relação com responsável pelo domicílio")
 
-    class Meta:
-        verbose_name = "Informação de morador"
-        verbose_name_plural = "Informações de moradores"
-
-    def __str__(self):
-        return f"{self.nome_morador} {self.sobrenome_morador}, Sexo: {self.sexo}, Relação com responsável: {self.relacao_com_responsavel}"
-
-class CaracteristicasDomicilio(models.Model):
-    ABASTECIMENTO_CHOICES = [
-        (1, 'Rede geral de distribuição'),
-        (2, 'Profundo ou artesiano'),
-        (3, 'Raso, freático ou cacimba'),
-        (4, 'Fonte, nascente ou mina'),
-        (5, 'Carro-pipa'),
-        (6, 'Água da chuva armazenada'),
-        (7, 'Rios, açudes, córregos, lagos e igarapés'),
-        (8, 'Outra'),
+    REGISTRO_DE_NASCIMENTO_CHOICES = [
+        ('do_cartorio', 'Do cartório'),
+        ('registro_administrativo_de_nascimento_indígena', 'Registro administrativo de nascimento indígena'),
+        ('nao_tem', 'Não tem'),
+        ('nao_sabe', 'Não sabe'),
     ]
-    abastecimento = models.IntegerField(choices=ABASTECIMENTO_CHOICES)
+    registro_de_nascimento = models.CharField(max_length=2, choices=REGISTRO_DE_NASCIMENTO_CHOICES, verbose_name="Tem registro de nascimento?")
 
-    def __str__(self):
-        return self.abastecimento
-
-    ACESSO_REDE_CHOICES = [
-        (1, 'Sim'),
-        (2, 'Nao'),
+    NUPICIALIDADE_CHOICES = [
+        ('possui_conjuge_ou_companheiro(a)', 'Possui conjuge ou companheiro(a)'),
+        ('sim', 'Sim'),
+        ('nao', 'Não'),
     ]
-    acesso_rede_geral_agua = models.IntegerField(choices=ACESSO_REDE_CHOICES)
+    nupicialidade = models.CharField(max_length=2, choices=NUPICIALIDADE_CHOICES, verbose_name="Nupicialidade")
+
+    VIVEM_COMPANHIA_CHOICES = [
+        ('sim', 'Sim'),
+        ('nao', 'Não'),
+    ]
+    vivem_companhia = models.CharField(max_length=2, choices=VIVEM_COMPANHIA_CHOICES, verbose_name="Vivem com companhia de conjuge ou companheiro(a)?")
+
+    nome_conjuge = models.CharField(max_length=200, verbose_name="Nome do cônjuge", blank=True, null=True)
+
+    TIPO_UNIAO_CHOICES = [
+        ('casamento_civil_e_religioso', 'Casamento civil e religioso'),
+        ('so_casamento_civil', 'So casamento civil'),
+        ('so_casamento_religioso', 'So casamento religioso'),
+        ('uniao_consensual', 'União consensual'),
+    ]
+    tipo_uniao = models.CharField(max_length=2, choices=TIPO_UNIAO_CHOICES, verbose_name="Tipo de união")
+
+    TRABALHOU_OU_ESTAGIOU_CHOICES = [
+    ("SIM", "Sim"),
+    ("NAO", "Não"),
+    ]
+    trabalhou_ou_estagiou = models.CharField(max_length=3, choices=TRABALHOU_OU_ESTAGIOU_CHOICES, verbose_name="Trabalhou ou estagiou em alguma atividade remunerada em dinheiro?")
+
+    NUMERO_DE_TRABALHOS_CHOICES = [
+    ("um", "1"),
+    ("dois", "2"),
+    ("tres_ou_mais", "3 ou mais"),
+    ]
+    numero_de_trabalhos = models.CharField(max_length=3, choices=NUMERO_DE_TRABALHOS_CHOICES, verbose_name="Quantos trabalhos tinha nos últimos meses?")
     
-    def __str__(self):
-        return self.acesso_rede_geral_agua
+    ocupacao_funcao = models.StringField(max_length=400, verbose_name="Qual era a ocupação, cargo ou função que tinha nesse trabalho?")
 
-    AGUA_UTILIZADA_CHEGA_CHOICES = [
-        (1, 'Encanada até dentro de casa, apartamento ou habitação'),
-        (2, 'Encanada, mas apenas no terreno'),
-        (3, 'Não chega encanada'),
-    ]
-    agua_utilizada_chega = models.IntegerField(choices=AGUA_UTILIZADA_CHEGA_CHOICES)
-
-    def __str__(self):
-        return self.agua_utilizada_chega
-
-    quantidade_banheiros_exclusivos_com_chuveiro_vaso = models.CharField(max_length=2)
-
-    def __str__(self):
-        return self.quantidade_banheiros_exclusivos_com_chuveiro_vaso
-
-    UTILIZA_BANHEIRO_COMUM_CHOICEs = [
-        (1, 'Sim'),
-        (2, 'Nao'),
-    ]
-    utiliza_banheiro_comum = models.IntegerField(choices=UTILIZA_BANHEIRO_COMUM_CHOICEs)
-
-    def __str__(self):
-        return self.utiliza_banheiro_comum
-
-    UTILIZA_SANITARIOS_BURACOS_DEJECAO_CHOICES = [
-        (1, 'Sim'),
-        (2, 'Nao'),
-
-    ]
-    utiliza_sanitarios_buracos_dejeicao = models.IntegerField(choices=UTILIZA_SANITARIOS_BURACOS_DEJECAO_CHOICES)
-
-    def __str__(self):
-        return self.utiliza_sanitarios_buracos_dejeicao
-
-    ONDE_VAI_ESGOTO_BANHEIRO_CHOICES = [
-        (1, 'Rede geral ou pluvial'),
-        (2, 'Ligada a rede'),
-        (3, 'Nao ligada a rede'),
-        (4, 'Fossa rudimentar ou buraco'),
-        (5, 'Vala'),
-        (6, 'Rio, lago, córrego ou mar'),
-        (7, 'Outra forma'),
-    ]
-    onde_vai_esgoto_banheiro = models.IntegerField(choices=ONDE_VAI_ESGOTO_BANHEIRO_CHOICES)
-
-    def __str__(self):
-        return self.onde_vai_esgoto_banheiro
-
-    ONDE_VAI_ESGOTO_SANITARIO_BURACO_CHOICES = [
-        (1, 'Rede geral ou pluvial'),
-        (2, 'Ligada a rede'),
-        (3, 'Nao ligada a rede'),
-        (4, 'Fossa rudimentar ou buraco'),
-        (5, 'Vala'),
-        (6, 'Rio, lago, córrego ou mar'),
-        (7, 'Outra forma'),
-    ]
-    onde_vai_esgoto_sanitario_buraco = models.IntegerField(choices=ONDE_VAI_ESGOTO_SANITARIO_BURACO_CHOICES)
-
-    def __str__(self):
-        return self.onde_vai_esgoto_sanitario_buraco
-
-    LIXO_CHOICES = [
-        (1, 'Coletado no domicilio por serviços de limpeza'),
-        (2, 'Depositado em cacamba de serviço de limpeza'),
-        (3, 'Queimado na propriedade'),
-        (4, 'Enterrado na propriedade'),
-        (5, 'Jogado em terreno baldio, encosta ou área pública'),
-        (6, 'Outro destino'),
-    ]
-    lixo = models.IntegerField(choices=LIXO_CHOICES)
-
-    def __str__(self):
-        return self.lixo
+    atividade_negocio = models.StringField(max_length=400, verbose_name="Qual era a principal atividade do negócio ou empresa em que tinha esse trabalho?")
     
+    CARTEIRA_ASSINADA_CHOICES = [
+    ("sim", "Sim"),
+    ("nao", "Não"),
+    ]
+    carteira_assinada = models.CharField(max_length=3, choices=CARTEIRA_ASSINADA_CHOICES, verbose_name="Nesse trabalho tinha carteira de trabalho assinada?")
 
-    
-    #PAGINA 4
+    EMPRESA_COM_CNPJ_CHOICES = [
+    ("sim", "Sim"),
+    ("nao", "Não"),
+    ]
+    empresa_com_cnpj = models.CharField(max_length=3, choices=EMPRESA_COM_CNPJ_CHOICES, verbose_name="Esse negócio ou empresa era registrado no cadastro nacional de pessoa jurídica - cnpj?")
+
+    FAIXA_DE_RENDIMENTO_CHOICES = [
+    ("faixa_1_ate_500", "1 - 1,00 a 500,00"),
+    ("faixa_2_501_a_1000", "2 - 501,00 a 1.000,00"),
+    ("faixa_3_1001_a_2000", "3 - 1.001,00 a 2.000,00"),
+    ("faixa_4_2001_a_3000", "4 - 2.001,00 a 3.000,00"),
+    ("faixa_5_3001_a_5000", "5 - 3.001,00 a 5.000,00"),
+    ("faixa_6_5001_a_10000", "6 - 5.001,00 a 10.000,00"),
+    ("faixa_7_10001_a_20000", "7 - 10.001,00 a 20.000,00"),
+    ("faixa_8_20001_a_100000", "8 - 20.001,00 a 100.000,00"),
+    ("faixa_9_mais_de_100000", "9 - 100.001 ou mais"),
+    ]
+    faixa_de_rendimento = models.CharField(max_length=25, choices=FAIXA_DE_RENDIMENTO_CHOICES, verbose_name="Faixa de rendimento")
+
+    FALECEU_ALGUEM_CHOICES = [
+    ("sim", "Sim"),
+    ("nao", "Não"),
+    ]
+    faleceu_algum = models.CharField(max_length=3, choices=FALECEU_ALGUEM_CHOICES, verbose_name="Faleceu alguma pessoa que morava com você(s)? (inclusive recém-nascidos e idosos)")
+
 class CaracteristicasAdicionaisMoradores(models.Model):
     cor_raca_CHOICES = [
         (1, 'Branca'),
