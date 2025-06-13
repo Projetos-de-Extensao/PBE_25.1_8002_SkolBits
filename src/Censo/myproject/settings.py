fd4db09a193edbe38743c402bab529cc2eb45ad7
fabriciobrito01 <fabriciodbe@gmail.com>
2025-06-13 09:59:41 -0300

    cors

--- src/Censo/myproject/settings.py
+++ src/Censo/myproject/settings.py
@@ -52,9 +52,11 @@ INSTALLED_APPS = [
     'myapp',
     'drf_yasg',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -63,6 +65,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'myproject.urls'
 
